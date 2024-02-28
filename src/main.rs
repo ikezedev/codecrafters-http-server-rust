9cf059b2f0e415bf9a565196f1c2eae70b38c493
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2024-02-28 19:38:50 +0100

    2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let _ = stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
             }
             Err(e) => {
                 println!("error: {}", e);
